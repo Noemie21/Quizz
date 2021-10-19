014563f83fbd32df313bb5c3c4cef69adff0e43b
ChaimaBenmakhlouf <chaima.benmakhlouf@supinternet.fr>
2021-10-19 14:13:21 +0200

    add question part

--- quizz/questions/models.py
+++ quizz/questions/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.expressions import F
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    content = models.CharField(max_length=200)
+    answer = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    howManyTime = models.IntegerField()
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.content
--- quizz/questions/views.py
+++ quizz/questions/views.py
@@ -1,16 +1,45 @@
 
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from .models import *
+from .forms import *
+
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            user = authenticate(username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
+            user = authenticate(
+                username=form.cleaned_data["username"], password=form.cleaned_data["password1"])
             login(request, user)
             return redirect("")
     else:
         form = UserCreationForm()
-    return render(request, "registration/register.html", context={"form":form})
+    return render(request, "registration/register.html", context={"form": form})
+
+
+def home(request):
+    current_user = request.user
+
+    questions = Question.objects.filter(user=current_user.id)
+    form = QuestionForm()
+    context = {'questions': questions, 'form': form}
+    return render(request, 'form/addQuestionForm.html', context)
+
+
+def index(request):
+    current_user = request.user
+
+    questions = Question.objects.filter(user=current_user.id)
+    form = QuestionForm()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            localForm = form.save(commit=False)
+            localForm.user = request.user
+            localForm.save()
+        return redirect('/')
+    context = {'questions': questions, 'form': form}
+    return render(request, 'form/addQuestionForm.html', context)
